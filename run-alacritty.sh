4d55165d5ac0c68a023e7a4af08e88ffe80dc722
Pouya Sanooei <sourcesoft@users.noreply.github.com>
2021-07-07 22:58:02 -0400

    Create run-alacritty.sh

---
+++ run-alacritty.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+wid=$(xdotool search --onlyvisible --class Alacritty)
+
+echo $wid
+
+if [[ -z "$wid" ]]; then
+	run-alacritty
+fi
+
+
+activews=$(xdotool get_desktop)
+activeid=$(xdotool getactivewindow)
+if [[ "$wid" == "$activeid" ]]; then
+	xdotool windowminimize $wid
+else
+	xdotool set_desktop_for_window $wid $activews
+	sleep 0.1
+	xdotool windowactivate $wid
+fi
