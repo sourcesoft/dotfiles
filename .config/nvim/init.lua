ec9a4376c2c09876331fe895e706d838aec462de
Pouya Sanooei <pooya.source@gmail.com>
2024-07-07 18:05:26 -0400

    fix dashboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,108 +1,15 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
 -- Make line numbers default
 vim.opt.number = true
+
 -- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -112,7 +19,6 @@ vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
@@ -133,7 +39,7 @@ vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 200
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -143,8 +49,11 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
+
+-- Set the characters to display for whitespace
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Set the tab width to 4 (instead of default 8 which is too big for Golang)
 vim.opt.tabstop = 4
 
 -- Preview substitutions live, as you type!
@@ -155,10 +64,9 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-vim.opt.scroll = 5
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+-- Scroll jump when pressing <C-u> or <C-d>
+vim.opt.scroll = 5
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -286,7 +194,6 @@ require('lazy').setup({
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
@@ -872,9 +779,9 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
@@ -908,3 +815,32 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- clean empty and unwanted buffers before exiting and saving session
+function cleanEmptyBuffers()
+  -- Get a list of all windows
+  local windows = vim.api.nvim_list_wins()
+
+  -- Iterate over all windows
+  for _, win in ipairs(windows) do
+    -- Check if the window is valid
+    if vim.api.nvim_win_is_valid(win) then
+      -- Get the buffer associated with the window
+      local buf = vim.api.nvim_win_get_buf(win)
+
+      -- Get the name of the buffer
+      local buf_name = vim.api.nvim_buf_get_name(buf)
+
+      -- Check if the buffer name matches unwanted patterns
+      if buf_name:match 'neo%-tree' or buf_name:match 'edgy://' or buf_name:match 'OUTLINE_' then
+        -- Close the window (and the buffer with it)
+        vim.api.nvim_win_close(win, true)
+      end
+    end
+  end
+end
+vim.api.nvim_create_user_command('CleanEmptyBuffers', cleanEmptyBuffers, {})
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'PersistenceSavePre',
+  callback = cleanEmptyBuffers,
+})
--- .config/nvim/lua/custom/plugins/dashboard.lua
+++ .config/nvim/lua/custom/plugins/dashboard.lua
@@ -4,34 +4,42 @@ return {
   dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   opts = function()
     local logo = [[
-       ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-       ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-       ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-       ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-       ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-       ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+
+ .----------------. .----------------. .----------------. .----------------. 
+| .--------------. | .--------------. | .--------------. | .--------------. |
+| |  _________   | | |     ____     | | |  _______     | | |     ____     | |
+| | |  _   _  |  | | |   .'    `.   | | | |_   __ \    | | |   .'    `.   | |
+| | |_/ | | \_|  | | |  /  .--.  \  | | |   | |__) |   | | |  /  .--.  \  | |
+| |     | |      | | |  | |    | |  | | |   |  __ /    | | |  | |    | |  | |
+| |    _| |_     | | |  \  `--'  /  | | |  _| |  \ \_  | | |  \  `--'  /  | |
+| |   |_____|    | | |   `.____.'   | | | |____| |___| | | |   `.____.'   | |
+| |              | | |              | | |              | | |              | |
+| '--------------' | '--------------' | '--------------' | '--------------' |
+ '----------------' '----------------' '----------------' '----------------' 
+
+       I know that I know nothing. - Socrates
   ]]
 
     logo = string.rep('\n', 8) .. logo .. '\n\n'
 
     local opts = {
-      theme = 'doom',
-      hide = {
-        -- this is taken care of by lualine
-        -- enabling this messes up the actual laststatus setting after loading a file
-        statusline = false,
-      },
+      theme = 'hyper',
       config = {
+        packages = { enable = false },
         header = vim.split(logo, '\n'),
-      -- stylua: ignore
-      center = {
-        { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-        { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-        { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-        { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-        { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
-      },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          { action = 'lua require("telescope.builtin").find_files()', desc = ' Find File', icon = ' ', key = 'f' },
+          { action = 'lua require("persistence").load()', desc = ' Restore Session', icon = ' ', key = 'r' },
+          {
+            action = function()
+              vim.api.nvim_input '<cmd>qa<cr>'
+            end,
+            desc = ' Quit',
+            icon = ' ',
+            key = 'q',
+          },
+        },
         footer = function()
           local stats = require('lazy').stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
@@ -39,24 +47,6 @@ return {
         end,
       },
     }
-
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
-      button.key_format = '  %s'
-    end
-
-    -- open dashboard after closing lazy
-    if vim.o.filetype == 'lazy' then
-      vim.api.nvim_create_autocmd('WinClosed', {
-        pattern = tostring(vim.api.nvim_get_current_win()),
-        once = true,
-        callback = function()
-          vim.schedule(function()
-            vim.api.nvim_exec_autocmds('UIEnter', { group = 'dashboard' })
-          end)
-        end,
-      })
-    end
 
     return opts
   end,
