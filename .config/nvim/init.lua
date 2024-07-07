3c12c4f4e500662fc36f76cec7da22c1602ef2c5
Pouya Sanooei <pooya.source@gmail.com>
2024-07-07 18:09:43 -0400

    add save shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,6 +72,9 @@ vim.opt.scroll = 5
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Save active buffer
+vim.keymap.set('n', '<leader>;', ':w!<CR>', { desc = 'Go to previous [D]iagnostic message' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
