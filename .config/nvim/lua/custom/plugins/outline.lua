e17b5e0538283bb9a29b2f0dc0b2dbe2324551dc
Pouya Sanooei <pooya.source@gmail.com>
2024-07-07 19:30:42 -0400

    add trouble

--- .config/nvim/lua/custom/plugins/outline.lua
+++ .config/nvim/lua/custom/plugins/outline.lua
@@ -5,7 +5,9 @@ return {
     vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
 
     require('outline').setup {
-      -- Your setup opts here (leave empty to use defaults)
+      outline_window = {
+        focus_on_open = true,
+      },
     }
   end,
 }
