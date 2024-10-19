3a9b09f091b3dd1fb5e1ec5fe12ee083931b28dd
Pouya Sanooei <pooya.source@gmail.com>
2024-10-19 19:49:55 -0400

    update nvim config

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '<C-e>', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
